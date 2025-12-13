952da9ca2c2b9d2469e9cbf534895f0f4f4afcbb
Tuấn Anh <146856535+tnnhuaa@users.noreply.github.com>
2025-12-13 20:35:57 +0700

    fix: crash backend (#28)

--- src/backend/src/repositories/projectRepository.js
+++ src/backend/src/repositories/projectRepository.js
@@ -14,20 +14,27 @@ class ProjectRepository {
         return project.save();
     }
 
+    async findProjectsByManager(managerId) {
+        return Project.find({ manager: managerId });
+    }
     async findProjectById(id) {
-        return Project.findById(id).populate('manager', 'username email');
+        return Project.findById(id).populate("manager", "username email");
     }
 
     async findAllProjects() {
-        return Project.find().populate('manager', 'username email');
+        return Project.find().populate("manager", "username email");
     }
 
     async findProjectsById(ids, session = null) {
-        return Project.find({ _id: { $in: ids } }).populate('manager', 'username email').session(session);
+        return Project.find({ _id: { $in: ids } })
+            .populate("manager", "username email")
+            .session(session);
     }
 
     async updateProject(id, data, session = null) {
-        return Project.findByIdAndUpdate(id, data, { new: true }).session(session);
+        return Project.findByIdAndUpdate(id, data, { new: true }).session(
+            session
+        );
     }
 }
 
